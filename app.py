33237069ec6ec036ae4bb6664b79c66fce7da6b2
Jin Park <jin+github@jinpark.net>
2015-04-05 03:37:37 +0900

    initial commit

---
+++ app.py
@@ -0,0 +1,125 @@
+from flask import Flask, send_file, request, abort, jsonify, send_from_directory, make_response, redirect, current_app
+from StringIO import StringIO
+from wand.image import Image
+from urlparse import urlparse
+from tempfile import NamedTemporaryFile
+from shutil import copyfileobj
+from functools import wraps, update_wrapper
+from datetime import datetime
+import requests
+import os
+import logging
+
+from gevent import monkey; monkey.patch_all()
+
+app = Flask(__name__)
+stream_handler = logging.StreamHandler()
+app.logger.addHandler(stream_handler)
+app.logger.setLevel(logging.INFO)
+app.logger.info('jinageresizer startup')
+
+def nocache(view):
+    """
+    no cache decorator. used for health check
+    """
+    @wraps(view)
+    def no_cache(*args, **kwargs):
+        response = make_response(view(*args, **kwargs))
+        response.headers['Last-Modified'] = datetime.now()
+        response.headers['Cache-Control'] = 'no-store, no-cache, must-revalidate, post-check=0, pre-check=0, max-age=0'
+        response.headers['Pragma'] = 'no-cache'
+        response.headers['Expires'] = '-1'
+        return response
+
+    return update_wrapper(no_cache, view)
+
+@app.route('/')
+def home():
+    return redirect("https://github.com/jinpark/imageresizer")
+
+@app.route('/favicon.ico/')
+def favicon():
+    """
+    I hate favicons. ugh
+    """
+    return send_from_directory(os.path.join(app.root_path, 'static'),
+                               'favicon.ico', mimetype='image/png')
+
+@app.route('/<path:url>/')
+def convert(url):
+    query_string = request.args
+    try:
+        r = requests.get(url, timeout=1)
+        filename, file_ext = os.path.splitext(os.path.basename(urlparse(url).path))
+        if 'image' not in r.headers['content-type']:
+            app.logger.error(url + " is not an image.")
+            abort(400)
+    except:
+        app.logger.exception("Error while getting url: " + url)
+        abort(400)
+    try:
+        with Image(file=StringIO(r.content)) as img:
+            if 'type' in query_string.keys() and query_string['type'] in ['jpeg', 'jpg', 'png']:
+                img.format = query_string['type']
+            if 'rwidth' in query_string.keys():
+                try:
+                    resize_width = int(query_string['rwidth'])
+                except:
+                    app.logger.exception("rwidth is invalid: " + query_string['rwidth'])
+                    return bad_request(bad_var_name='rwidth')
+            else:
+                resize_width = None
+            if 'rheight' in query_string.keys():
+                try:
+                    resize_height = int(query_string['rheight'])
+                except:
+                    app.logger.exception("rheight is invalid: " + query_string['rheight'])
+                    return bad_request(bad_var_name='rheight')
+            else:
+                resize_height = None
+            if resize_width and resize_height:
+                img.resize(resize_width, resize_height)
+            if resize_width and not resize_height:
+                img.transform(resize=str(resize_width))
+            if resize_height and not resize_width:
+                img.transform(resize='x' + str(resize_height))
+
+            temp_file = NamedTemporaryFile(mode='w+b',suffix=img.format)
+            img.save(file=temp_file)
+            temp_file.seek(0,0)
+            response = send_file(temp_file, mimetype=img.mimetype)
+            return response
+    except:
+        app.logger.exception("Error while getting image for wand")
+        abort(500)
+
+
+# on hold until I can figure out gravity
+# def crop(img, width, height):
+#     """
+#     Crops image based on left, top, bottom, right pixel sizes.
+#     Default gravity is center. Options are center, north, north_east, ...
+#     View http://docs.wand-py.org/en/latest/wand/image.html#wand.image.BaseImage.crop
+#     for more details
+#     """
+#     img.transform("{}x{}".format(width,height))
+
+
+@app.route('/health/')
+@nocache
+def health_check():
+    return jsonify({'health': 'ok', 'commit_hash': os.environ.get('COMMIT_HASH')})
+
+
+@app.errorhandler(400)
+def bad_request(bad_var_name="", error=None):
+    message = {
+            'status': 400,
+            'message': 'Bad request: {0} is not valid'.format(bad_var_name),
+    }
+    resp = jsonify(message)
+    resp.status_code = 400
+    return resp
+
+if __name__ == '__main__':
+    app.run()
---
+++ tests/test_app.py
@@ -0,0 +1,111 @@
+import os
+import sys
+import unittest
+import requests
+from freezegun import freeze_time
+from datetime import datetime
+from StringIO import StringIO
+from wand.image import Image
+from mock import MagicMock
+
+
+topdir = os.path.join(os.path.dirname(__file__), "..")
+sys.path.append(topdir)
+import app
+
+
+def requests_stub(*args, **kwargs):
+    response = requests.get.return_value
+    if args[0] == "http://badrequest.com/g/50/200":
+        raise
+    elif args[0] == "http://notanimage.com/g/50/200":
+        response.content = "thisisavideonotanimage"
+        response.headers = {'content-type': 'video/avi'}
+        return response
+    elif args[0] == "http://badimage.com/g/50/200":
+        response.content = "this is some bad image data"
+        response.headers = {'content-type': 'image/jpeg'}
+        return response
+    else:
+        with open('tests/images/50x200_pre.jpeg', 'r') as f:
+            response.content = f.read()
+            response.headers = {'content-type': 'image/jpeg'}
+            return response
+
+requests.get = MagicMock(side_effect=requests_stub)
+
+class TestImageResizerImageEdits(unittest.TestCase):
+    def setUp(self):
+        self.app = app.app.test_client()
+
+    def test_resize_width(self):
+        rv = self.app.get('/http://placekitten.com/g/50/200/?rwidth=100')
+        with Image(file=StringIO(rv.data)) as img:
+            self.assertEqual(img.width, 100)
+            self.assertEqual(img.height, 400)
+
+    def test_resize_height(self):
+        rv = self.app.get('/http://placekitten.com/g/50/200/?rheight=100')
+        with Image(file=StringIO(rv.data)) as img:
+            self.assertEqual(img.width, 25)
+            self.assertEqual(img.height, 100)
+
+    def test_resize_width_and_height(self):
+        rv = self.app.get('/http://placekitten.com/g/50/200/?rheight=100&rwidth=100')
+        with Image(file=StringIO(rv.data)) as img:
+            self.assertEqual(img.width, 100)
+            self.assertEqual(img.height, 100)
+
+    def test_convert_type(self):
+        rv = self.app.get('/http://placekitten.com/g/50/200/?type=png')
+        with Image(file=StringIO(rv.data)) as img:
+            self.assertEqual(img.mimetype, "image/png")
+
+    def test_bad_request(self):
+        rv = self.app.get('/http://badrequest.com/g/50/200/?rwidth=100')
+        self.assertEqual(rv.status, "400 BAD REQUEST")
+
+    def test_not_an_image(self):
+        rv = self.app.get('/http://notanimage.com/g/50/200/?rwidth=100')
+        self.assertEqual(rv.status, "400 BAD REQUEST")
+
+    def test_bad_image(self):
+        rv = self.app.get('/http://badimage.com/g/50/200/?rwidth=100')
+        self.assertEqual(rv.status, "500 INTERNAL SERVER ERROR")
+
+    def test_bad_width(self):
+        rv = self.app.get('/http://placekitten.com/g/50/200/?rwidth=cat')
+        self.assertEqual(rv.status, "400 BAD REQUEST")
+
+    def test_bad_height(self):
+        rv = self.app.get('/http://placekitten.com/g/50/200/?rheight=cat')
+        self.assertEqual(rv.status, "400 BAD REQUEST")
+
+class TestImageResizerExtras(unittest.TestCase):
+    def setUp(self):
+        self.app = app.app.test_client()
+
+    def test_home_redirect(self):
+        rv = self.app.get('/', follow_redirects=False)
+        self.assertEqual(rv.location, "https://github.com/jinpark/imageresizer")
+
+    def test_health_check(self):
+        rv = self.app.get('/health/')
+        self.assertEqual(rv.data, '{\n  "commit_hash": null, \n  "health": "ok"\n}')
+
+    @freeze_time("2015-03-29")
+    def test_health_check_uncached(self):
+        rv = self.app.get('/health/')
+        headers = rv.headers
+        datetime_now = datetime(2015, 3, 29)
+        self.assertEqual(headers['Last-Modified'], str(datetime_now))
+        self.assertEqual(headers['Cache-Control'], 'no-store, no-cache, must-revalidate, post-check=0, pre-check=0, max-age=0')
+        self.assertEqual(headers['Pragma'], 'no-cache')
+        self.assertEqual(headers['Expires'], '-1')
+
+    def test_favicon(self):
+        rv = self.app.get('/favicon.ico/')
+        self.assertEqual(rv.mimetype, 'image/png')
+
+if __name__ == '__main__':
+    unittest.main()
